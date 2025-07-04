a995c3fd44da453ffd88036c627d61e9bf1c689b
TimeMachine1996 <clx19961996@126.com>
2025-07-04 16:55:32 +0800

    modify main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,16 @@
+# 定义变量
+variable "vpc_name" {
+  description = "VPC的名称"
+  type        = string
+  default     = "terraform-vpc"  # 默认值
+}
+
+variable "vswitch_name" {
+  description = "交换机的名称"
+  type        = string
+  default     = "terraform-vsw"  # 默认值
+}
+
 # 配置阿里云Provider
 provider "alicloud" {
   # 在这里配置您的认证信息
@@ -8,13 +21,13 @@ provider "alicloud" {
 
 # 创建VPC
 resource "alicloud_vpc" "vpc" {
-  vpc_name   = "terraform-vpc"  # VPC名称
+  vpc_name   = var.vpc_name     # 使用变量
   cidr_block = "10.0.0.0/16"    # VPC的网段
 }
 
 # 创建交换机(vSwitch)
 resource "alicloud_vswitch" "vsw" {
-  vswitch_name      = "terraform-vsw"
+  vswitch_name      = var.vswitch_name  # 使用变量
   vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   zone_id           = "cn-hangzhou-b"  # 可用区ID
